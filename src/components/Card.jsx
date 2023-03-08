bd5f37b088474336b7875143321503927f023e8d
alajido <96017433+Alajido@users.noreply.github.com>
2023-03-08 20:14:43 +0100

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export const Card = () => {
+export const Card = (props) => {
   return (
     <div>
-        <div className='border'>
-            <h1>Comfortably</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, perspiciatis?</p>
+        <div className='flex max-w-md overflow-hidden bg-white py-3 px-10 md:px-0 my-3 text-center'>
+            <div className=''>
+            <h1 className=' font-bold text-[18px] '>{props.title}</h1>
+            <p className=' font-normal'>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit. adipisicing elit. Qui, perspiciatis?</p>
+            </div>
         </div>
     </div>
   )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,11 +9,14 @@ export const Main = () => {
   //     </div>
   //   </div>
   return (
-    <section className='relative bg-[#FFFFFF] min-h-[350px] border-2  capitalize'>
+    <section className='relative flex-col justify-center items-center bg-[#FFFFFF] min-h-[250px] border-2 capitalize'>
+        <h1 className='text-[#000000] text-center font-bold my-8 px-1 text-[24px]'>you can get them all for free now!</h1>
+      
       <div className="container border-2 mx-auto md:w-[70%]">
-        <h1 className='text-[#000000] text-center font-bold text-[28px]'>you can get them all for free now!</h1>
-        <div className="card">
-          <Card></Card>
+        <div className="flex flex-col md:flex-row justify-evenly card mx-auto">
+          <Card title='comfortably'></Card>
+          <Card title='easily'></Card>
+          <Card title='reliable'></Card>
         </div>
 
       </div>
