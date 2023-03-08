d96b6c51bb3000469f3b9c15c2f9ceb6c3e46d7c
alajido <96017433+Alajido@users.noreply.github.com>
2023-03-08 22:01:35 +0100

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 export const Card = (props) => {
+    useEffect(() => {
+        Aos.init({
+            duration: 1000,
+            easing: 'ease-out',
+            delay: 100,
+        });
+    }, [])
   return (
+    
     <div>
-        <div className='flex max-w-md overflow-hidden bg-white py-3 px-10 md:px-0 my-3 text-center'>
+        <div data-aos="fade-up" className='flex max-w-md overflow-hidden bg-white py-3 px-10 md:px-3 my-3 text-center'>
             <div className=''>
-            <h1 className=' font-bold text-[18px] '>{props.title}</h1>
-            <p className=' font-normal'>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit. adipisicing elit. Qui, perspiciatis?</p>
+                <div className='flex items-center justify-center'>
+                    {props.icon}
+                    <h1 className='font-bold text-[18px] px-4'>{props.title}</h1>
+                </div>
+                <p className=' font-normal'>Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit. adipisicing elit. Qui, perspiciatis?</p>
             </div>
         </div>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Card } from './Card';
+import { MdChair } from 'react-icons/md';
+import { MdSwapHorizontalCircle } from 'react-icons/md';
+import { MdFactCheck } from 'react-icons/md'
 
 export const Main = () => {
-
-  // <div className='relativebg-red-500 min-h-[400px] py-[100px] md:px-[20vh]'>
-  //     <div className='py-[100px] md:px-[20vh] border mx-auto'>
-  //       <h1>hello </h1>
-  //     </div>
-  //   </div>
   return (
     <section className='relative flex-col justify-center items-center bg-[#FFFFFF] min-h-[250px] border-2 capitalize'>
-        <h1 className='text-[#000000] text-center font-bold my-8 px-1 text-[24px]'>you can get them all for free now!</h1>
+      <h1 className='text-[#000000] text-center font-bold my-8 px-1 text-[24px]'>you can get them all for free now!</h1>
       
       <div className="container border-2 mx-auto md:w-[70%]">
         <div className="flex flex-col md:flex-row justify-evenly card mx-auto">
-          <Card title='comfortably'></Card>
-          <Card title='easily'></Card>
-          <Card title='reliable'></Card>
+          <Card title='comfortably' icon={<MdChair />}></Card>
+          <Card title='easily' icon={< MdSwapHorizontalCircle /> }></Card>
+          <Card title='reliable' icon={<MdFactCheck/>}></Card>
         </div>
 
       </div>
