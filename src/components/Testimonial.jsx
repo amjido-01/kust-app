0bcbe5ad7d56ac777e4b62cb90cfb64024e3367e
alajido <96017433+Alajido@users.noreply.github.com>
2023-03-11 08:04:10 +0100

    commit

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,22 +1,15 @@
-// import Swiper core and required modules
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 import { Testimony } from './Testimony';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import computer from '../assets/computer.jpg';
-// import { IoChevronForwardSharp } from 'react-icons/io';
-// import { FaChevronLeft } from 'react-icons/fa'
-// import { FaHandHoldingHeart } from 'react-icons/fa'
-import right from '../assets/Vector.png';
-
-
-
-// Import Swiper styles
+// import right from '../assets/Vector.png';
+import left from '../assets/left.png'
+import right from '../assets/right.png'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-// Swiper.use([Autoplay])
 
 export const Testimonial = () => {
 
@@ -43,65 +36,70 @@ export const Testimonial = () => {
         }
     ]
 
-  return (
-    <Swiper className='md:w-[65%] overflow-hidden min-h-[200px] my-10 px-8 md:px-4'
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-      autoplay={{
-        delay: 4000,
-        disableOnInteraction: false,
-      }}
-    //   spaceBetween={20}
-      slidesPerView={3}
-      navigation={{
-        prevEl: '.prev-button',
-        nextEl: '.next-button',
-      }}
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      breakpoints={{
-            200: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            },
-          // when window width is >= 640px
-          400: {
-            slidesPerView: 1,
-            spaceBetween: 10
-          },
-          // when window width is >= 768px
-          640: {
-            slidesPerView: 2,
-          },
-          // when window width is >= 1024px
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 10
-          },
-          1583: {
-            slidesPerView: 3,
-            spaceBetween: 30
-          },
-      }}
-    >
-        <div className='prev-button absolute top-10 text-red-700 z-10 w-4 h-4'>
-        <img src={right} alt='Previous'/>
-      </div>
-      <div className='next-button'>
-        <img src={right} alt='Next'/>
-      </div>
-      <SwiperSlide className='flex items-center justify-center'>
-        <Testimony name={data[3].name} dept={data[3].dept} img={computer}/>
-      </SwiperSlide>
-      <SwiperSlide className='flex items-center justify-center'><Testimony name={data[0].name} dept={data[0].dept} img={computer}/></SwiperSlide>
-      <SwiperSlide className='flex items-center justify-center'><Testimony name={data[1].name} dept={data[1].dept} img={computer}/></SwiperSlide>
-      <SwiperSlide className='flex items-center justify-center'><Testimony name={data[1].name} dept={data[1].dept} img={computer}/></SwiperSlide>
-      <SwiperSlide className='flex items-center justify-center'><Testimony name={data[1].name} dept={data[1].dept} img={computer}/></SwiperSlide>
-      <SwiperSlide className='flex items-center justify-center'><Testimony name={data[2].name} dept={data[2].dept} img={computer}/></SwiperSlide>
-      <SwiperSlide className='flex items-center justify-center'><Testimony name={data[2].name} dept={data[2].dept} img={computer}/></SwiperSlide>
-      <SwiperSlide className='flex items-center justify-center'><Testimony name={data[2].name} dept={data[2].dept} img={computer}/></SwiperSlide>
-    </Swiper>
-  );
+    return (
+        <section className='md:w-[70%] relative overflow-hidden mx-auto min-h-[200px] py-5 px-4 my-10'>
+            <Swiper className='container mx-auto relative'
+            
+                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+                autoplay={{
+                    delay: 4000,
+                    disableOnInteraction: false,
+                }}
+                slidesPerView={3}
+                navigation={{
+                    prevEl: '.prev-button',
+                    nextEl: '.next-button',
+                }}
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                breakpoints={{
+                    // >=200
+                    200: {
+                        slidesPerView: 1,
+                        spaceBetween: 10
+                    },
+                    // >= 40px
+                    400: {
+                        slidesPerView: 1,
+                        spaceBetween: 10
+                    },
+                    // >= 640px
+                    640: {
+                        slidesPerView: 2,
+                    },
+                    // >= 1024px
+                    1024: {
+                        slidesPerView: 3,
+                        spaceBetween: 20
+                    },
+                    // >= 1583px
+                    1583: {
+                        slidesPerView: 3,
+                        spaceBetween: 30
+                    },
+                }}
+                >
+
+                {/* <div className='next-button absolute top-20 z-10 -right-2'>
+                    <img src={left} alt='Next'/>
+                </div>
+                
+                <div className='next-button'>
+                    <img src={right} alt='Next'/>
+                </div> */}
+                <SwiperSlide className='flex items-center justify-center'>
+                    <Testimony name={data[3].name} dept={data[3].dept} img={computer}/>
+                </SwiperSlide>
+                <SwiperSlide className='flex items-center justify-center'><Testimony name={data[0].name} dept={data[0].dept} img={computer}/></SwiperSlide>
+                <SwiperSlide className='flex items-center justify-center'><Testimony name={data[1].name} dept={data[1].dept} img={computer}/></SwiperSlide>
+                <SwiperSlide className='flex items-center justify-center'><Testimony name={data[1].name} dept={data[1].dept} img={computer}/></SwiperSlide>
+                <SwiperSlide className='flex items-center justify-center'><Testimony name={data[1].name} dept={data[1].dept} img={computer}/></SwiperSlide>
+                <SwiperSlide className='flex items-center justify-center'><Testimony name={data[2].name} dept={data[2].dept} img={computer}/></SwiperSlide>
+                <SwiperSlide className='flex items-center justify-center'><Testimony name={data[2].name} dept={data[2].dept} img={computer}/></SwiperSlide>
+                <SwiperSlide className='flex items-center justify-center'><Testimony name={data[2].name} dept={data[2].dept} img={computer}/></SwiperSlide>
+            </Swiper>
+        </section>
+    );
 };
--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Testimony = ({ name, dept, img }) => {
     return (
-        <div className='inline-block max-w-[20rem] border-2 py-4 px-4 bg-[#D9D9D9] rounded-md'>
+        <div className='inline-block max-w-[20rem] py-4 px-4 bg-[#D9D9D9] rounded-md'>
             <div>
-                <p className='mt-2 text-[13px] text-[#000000]'>“  Lorem ipsum dolor sit amet consectetur. Dictum nunc mattis nisl eget sit in habitasse est amet. Eleifend et diam magna posuere lacus viverra tortor. ”</p>
+                <p className='mt-2 text-[13px] text-[#000000]'>“Lorem ipsum dolor sit amet consectetur. Dictum nunc mattis nisl eget sit in habitasse est amet. Eleifend et diam magna posuere lacus viverra tortor. ”</p>
             </div>
             <div className='flex items-center border mt-4'>
                 <img className='w-10 h-10 object-cover rounded-full border border-[#0F9D58]' src={img} />
