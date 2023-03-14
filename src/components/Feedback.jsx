eea7469715b8b896c38a7ee10edd1c5340b0d251
alajido <96017433+Alajido@users.noreply.github.com>
2023-03-14 07:32:49 +0100

    commit

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -4,25 +4,27 @@ import { RiSendPlane2Line } from "react-icons/ri";
 
 export const Feedback = () => {
   return (
-    <section className="bg-[#FFFFFF] text-gray-600 body-font relative border-4 border-green-600 min-h-[200px] px-8 md:px-4 ">
-      <div className="container border-2 md:w-[70%] mx-auto">
+    <section className="bg-[#FFFFFF] feedback text-gray-600 body-font relative px-8 md:px-4 ">
+      <div className="container md:w-[70%] mx-auto">
         <div className="w-full md:w-[45%] flex flex-col md:ml-aut md:py-8 mt-8 md:mt-0">
           <h2 className="text-[#000000] capitalize font-bold text-lg mb-1 title-font">
             want to say something
           </h2>
 
-          <div className="relative mb-4">
-            <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="leading-7 text-sm text-[#000000] font-normal"
+            >
               Email
             </label>
             <input
               type="text"
               id="name"
               name="name"
-              className="w-full bg-[#D9D9D9] px-3 font-normal py-1 placeholder:text-[#696969] focus:border-indigo-500 focus:ring-[1.5px] focus:ring-indigo-400 outline-none transition-colors duration-200 ease-in-out"
+              className="w-full bg-[#D9D9D9] px-3 font-normal py-1 md:py-2 placeholder:text-[#696969] focus:border-indigo-500 focus:ring-[1.5px] focus:ring-indigo-400 outline-none transition-colors duration-200 ease-in-out"
               placeholder="example@gmail.com"
             />
-            {/* focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 outline-none transition-colors duration-200 ease-in-out */}
           </div>
           <div className="relative mb-4">
             <label
@@ -36,20 +38,21 @@ export const Feedback = () => {
               id="email"
               name="email"
               placeholder="Joe Doe"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              className="w-full bg-[#D9D9D9] px-3 font-normal py-1 md:py-2 placeholder:text-[#696969] focus:border-indigo-500 focus:ring-[1.5px] focus:ring-indigo-400 outline-none transition-colors duration-200 ease-in-out"
             />
           </div>
           <div className="relative mb-4">
             <label
               htmlFor="message"
-              className="leading-7 text-sm text-gray-600"
+              className="leading-7 text-sm text-[#000000] font-normal"
             >
               Message
             </label>
             <textarea
               id="message"
               name="message"
-              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              placeholder="Your Message"
+              className="w-full bg-[#D9D9D9] px-3 font-normal py-1 md:py-2 placeholder:text-[#696969] focus:border-indigo-500 focus:ring-[1.5px] focus:ring-indigo-400 outline-none transition-colors duration-200 ease-in-out h-32"
             ></textarea>
           </div>
           <div>
