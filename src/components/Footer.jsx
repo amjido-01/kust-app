22a7068e38e692eb9407886a71e34d4acd313182
alajido <96017433+Alajido@users.noreply.github.com>
2023-03-15 15:39:26 +0100

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,43 +6,27 @@ export const Footer = () => {
       <footer className="bg-white dark:bg-gray-900">
         <div className="mx-auto w-full  border-2 border-white container p-4 sm:p-6">
           <div className="md:flex md:justify-between border-2 border-green-500">
-            {/* <div className="mb-6 md:mb-0 border-2 border-white">
-              <a href="https://flowbite.com/" className="flex items-center">
-                <img
-                  src="https://flowbite.com/docs/images/logo.svg"
-                  className="h-8 mr-3"
-                  alt="FlowBite Logo"
-                />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  Flowbite
-                </span>
-              </a>
-            </div> */}
-
-            <div className="grid grid-cols-2 gap-8 w-full sm:gap-6 sm:grid-cols-3  border-2 border-white">
+            <div className="flex foot mx-auto justify-between w-full md:w-[80%]">
               <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Resources
+                <h2 className="mb-2 text-sm text-[#FFFFFF]  font-semibold">
+                  KUST<span className="text-[#0F9D58]">Booklets</span>
                 </h2>
-                <ul className="text-gray-600 dark:text-gray-400">
-                  <li className="mb-4">
-                    <a href="https://flowbite.com/" className="hover:underline">
-                      Flowbite
+                <ul className=" font-medium text-[#F5F5F5] text-[12px] uppercase">
+                  <li className="">
+                    <a href="/" className="hover:underline">
+                      Home
                     </a>
                   </li>
                   <li>
-                    <a
-                      href="https://tailwindcss.com/"
-                      className="hover:underline"
-                    >
-                      Tailwind CSS
+                    <a href="#" className="hover:underline">
+                      About Us
                     </a>
                   </li>
                 </ul>
               </div>
               <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Follow us
+                <h2 className="mb-2 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                  DOcuments
                 </h2>
                 <ul className="text-gray-600 dark:text-gray-400">
                   <li className="mb-4">
@@ -50,7 +34,7 @@ export const Footer = () => {
                       href="https://github.com/themesberg/flowbite"
                       className="hover:underline "
                     >
-                      Github
+                      Handouts
                     </a>
                   </li>
                   <li>
@@ -58,24 +42,34 @@ export const Footer = () => {
                       href="https://discord.gg/4eeurUVvTy"
                       className="hover:underline"
                     >
-                      Discord
+                      past question
                     </a>
                   </li>
                 </ul>
               </div>
-              <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Legal
+              <div className="border-2">
+                <h2 className="mb-2 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                  follow us
                 </h2>
                 <ul className="text-gray-600 dark:text-gray-400">
-                  <li className="mb-4">
+                  <li className="">
                     <a href="#" className="hover:underline">
-                      Privacy Policy
+                      Niimatullah/ amjido_01
                     </a>
                   </li>
                   <li>
                     <a href="#" className="hover:underline">
-                      Terms &amp; Conditions
+                      muhammad Niimatullah/ amjido_01
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline">
+                      itx_prof/ amjido_01
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="hover:underline">
+                      itx_prof_niimatullahi/ amjido_01
                     </a>
                   </li>
                 </ul>
@@ -83,15 +77,14 @@ export const Footer = () => {
             </div>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-              © 2023{" "}
-              <a href="https://flowbite.com/" className="hover:underline">
-                Flowbite™
+          <div className="text-center">
+            <span className="text-sm text-[#FFFFFF] sm:text-center">
+              copyright © 2023{" "}
+              <a href="/" className="hover:underline">
+                MR PROFESSOR & AMJIDO_01™
               </a>
-              . All Rights Reserved.
             </span>
-            <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+            {/* <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
               <a
                 href="#"
                 className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
@@ -178,7 +171,7 @@ export const Footer = () => {
                 </svg>
                 <span className="sr-only">Dribbble account</span>
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
       </footer>
