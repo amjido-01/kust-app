beed09a2a9c167486d2d5aa0e85f0b2b3d23aeb4
alajido <96017433+Alajido@users.noreply.github.com>
2023-03-26 07:32:39 +0100

    commit

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { useState } from "react";
+
+const items = [
+  { id: 1, name: "Apple" },
+  { id: 2, name: "Banana" },
+  { id: 3, name: "Cherry" },
+];
 
 export const Documents = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredItems, setFilteredItems] = useState(items);
+
+  const handleSearch = (e) => {
+    const query = e.target.value;
+    setSearchQuery(query)
+
+    const filtered = items.filter((item) => 
+      item.name.toLowerCase().includes(query.toLowerCase())
+    )
+    setFilteredItems(filtered)
+  }
+
   return (
-    <section className="relative">
+    <section className="relative min-h-screen flex flex-col">
       <Header />
-      <div className="container w-[90%] md:w-[50%] mx-auto mt-[7rem] flex items-center justify-center">
+      <div className="container w-[90%] md:w-[50%] mx-auto mt-[7rem] flex items-center justify-center flex-1">
         <div className="text-center md:w-[70%]">
           <h1 className="text-[#1F1F1F] font-semibold md:font-bold text-[18px] md:text-[19px] leading-6">
             We are a team of two programmers and designers{" "}
@@ -21,12 +41,17 @@ export const Documents = () => {
 
       <div className="container w-[90%] md:w-[70%] border border-red-500 mx-auto my-10">
         <div className="container">
-          
+          <h1 className="text-center uppercase">search</h1>
+          <input type="text" value={searchQuery} onChange={handleSearch} placeholder="search"/>
+
+          {filteredItems.map((item) => 
+          <li key={item.id}>{item.name}</li>
+          ) }
         </div>
       </div>
 
       <div className="relative bottom-0">
-        <Footer className="absolute w-full" />
+        <Footer className="w-full" />
       </div>
     </section>
   );
