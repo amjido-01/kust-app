6738485991cf2d14898b139ebb4b217be1e85445
alajido <96017433+Alajido@users.noreply.github.com>
2023-03-26 16:18:28 +0100

    commit

--- src/components/Documents.jsx
+++ src/components/Documents.jsx
@@ -4,8 +4,7 @@ import { Footer } from "./Footer";
 import { useState } from "react";
 import { Data } from "../Data";
 
-
-console.log(Data)
+console.log(Data);
 
 export const Documents = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -13,38 +12,39 @@ export const Documents = () => {
 
   const handleSearch = (e) => {
     const query = e.target.value;
-    setSearchQuery(query)
+    setSearchQuery(query);
+    console.log(searchQuery)
 
-    const filtered = Data.filter((item) => 
+    const filtered = Data.filter((item) =>
       item.first_name.toLowerCase().includes(query.toLowerCase())
-    )
-    setFilteredItems(filtered)
-  }
+    );
+    setFilteredItems(filtered);
+  };
 
   return (
     <section className="relative min-h-screen flex flex-col">
       <Header />
-      <div className="container w-[90%] md:w-[50%] mx-auto mt-[7rem] flex items-center justify-center flex-1">
-        <div className="text-center md:w-[70%]">
-          <h1 className="text-[#1F1F1F] font-semibold md:font-bold text-[18px] md:text-[19px] leading-6">
-            We are a team of two programmers and designers{" "}
-          </h1>
-          <p className=" font-medium md:font-semibold text-[13px] md:text-[14px] mt-2">
-            From kano state, Nigeria. and a students of KUST wudil who dedicated
-            thier time to create some innovations and provide simplicity to our
-            esteemed fellow students of KUST Wudil.
-          </p>
-        </div>
+      <div className="container w-[90%] md:w-[50%] mx-auto mt-[7rem] flex-1">
+       <h1>welcome to the document page </h1>
       </div>
 
+    
       <div className="container w-[90%] md:w-[70%] border border-red-500 mx-auto my-10">
         <div className="container">
-          <h1 className="text-center uppercase">search</h1>
-          <input type="text" value={searchQuery} onChange={handleSearch} placeholder="search"/>
+          <h1 className="text-center uppercase">FACULTIES</h1>
+          <input
+            type="text"
+            className="border-2 border-green-500"
+            value={searchQuery}
+            onChange={handleSearch}
+            placeholder="search"
+          />
 
-          {filteredItems.map((item) => 
-          <li key={item.id}>{item.first_name}</li>
-          ) }
+          <ul className=" list-disc">
+            {filteredItems.map((item) => (
+              <a key={item.id} href="#"><li>{item.first_name}</li></a>
+            ))}
+          </ul>
         </div>
       </div>
 
