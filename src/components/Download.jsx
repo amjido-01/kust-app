b4885bad12086d17c3b4a65ce6334f2dcaffddbb
alajido <96017433+Alajido@users.noreply.github.com>
2023-04-27 19:31:23 +0100

    commit

---
+++ src/components/Download.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Download = () => {
+  return (
+        <button style={{fontFamily: 'Poppins'}} className='bg-[#755FFE] text-[#FFFFFF] text-[16px] font-medium leading-[24px] uppercase'>Download</button>
+  )
+}
--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Download } from './Download'
 
 export const File = () => {
   return (
     <div className='flex border-'>
-        <p className='bg-white text-[16px] font-medium leading-[24px] uppercase text-[#000000]'>introduction to computer science</p>
-        <button className='border-2'>download</button>
+        <p className='bg-white text-[16px] font-medium leading-[24px] uppercase text-[#000000] p-2'>introduction to computer science</p>
+        <Download></Download>
     </div>
  )
 }
