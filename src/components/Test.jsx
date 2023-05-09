b073a19fc577e61df13448412bc90ebde38d32b7
alajido <96017433+Alajido@users.noreply.github.com>
2023-05-09 13:50:13 +0100

    commit

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export const Test = () => {
+  const [show, setShow] = useState(false);
+
+  const handleDelete = () => {
+    setShow(true);
+  };
+
+  const handleProcced = () => {
+    setShow(false);
+  };
+
   return (
     <div>
-      
+      {show ? (
+        <div id="alert">
+          <h2>Are you sure?</h2>
+          <p>These changes can't be reverted!</p>
+          <button onClick={handleProcced}>Proceed</button>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <button onClick={handleDelete} className="border-2">
+        delete
+      </button>
     </div>
-  )
-}
+  );
+};
