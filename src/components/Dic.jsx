08f4173ba8c42ff60260284558745333d0834f5f
alajido <96017433+Alajido@users.noreply.github.com>
2023-05-10 23:32:12 +0100

    commit

--- src/components/Dic.jsx
+++ src/components/Dic.jsx
@@ -5,20 +5,20 @@ import axios from "axios";
 export const Dic = () => {
   const [word, setWord] = useState("");
   const [defination, setDefination] = useState("");
-  const [kalma, setKalma] = useState("")
-  const [pho, setPho] = useState("")
- 
+  const [test, setTest] = useState("")
+  const [pho, setPho] = useState("");
+
   const handleSearch = async () => {
     try {
       const res = await axios.get(
         `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
       );
       const data = res.data[0].meanings[0].definitions[0].definition;
-      const test = res.data[0].word
+      const test = res.data[0].word;
       console.log(data);
       console.log(test);
-      setDefination(data)
-      setKalma(test)
+      setDefination(data);
+      setWord("");
     } catch (error) {
       console.log(error);
     }
@@ -27,8 +27,13 @@ export const Dic = () => {
   const handleChange = (e) => {
     setWord(e.target.value);
   };
+
   return (
     <div>
+      <p>
+        Dictionary
+      </p>
+
       <input
         className="border-2 border-green-500"
         type="text"
@@ -36,8 +41,8 @@ export const Dic = () => {
         onChange={handleChange}
       />
       <button onClick={handleSearch}>search</button>
+      <p>{word}</p>
       <p>{defination}</p>
-      <p>{kalma}</p>
     </div>
   );
 };
