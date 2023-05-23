eb673fd7dbffb11e509dc1d50fccc93cc7777b7e
alajido <96017433+Alajido@users.noreply.github.com>
2023-05-23 22:45:45 +0100

    upload page

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,10 +3,11 @@ import axios from "axios";
 import { useState } from "react";
 
 export const Books = () => {
-  const test = import.meta.env.VITE_YOUR_GOOGLE_API_KEY;
+  //   const test = import.meta.env.VITE_YOUR_GOOGLE_API_KEY;
   const [query, setQuery] = useState("");
   const [books, setBooks] = useState([]);
-  const [freeBooks, setFreeBooks] = useState([]);
+  const [message, setMessage] = useState("")
+  //   const [freeBooks, setFreeBooks] = useState([]);
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
@@ -20,22 +21,29 @@ export const Books = () => {
 
     try {
       const response = await axios.get(
-        `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+        `https://openlibrary.org/search.json?title=${encodeURIComponent(
           query
-        )}&key=${test}`
+        )}&limit=10`
       );
       const res = response.data;
       console.log(res);
-      setBooks(res.items || []);
+      const filteredBooks = res.docs.filter((book) => book.cover_i)
+      if (filteredBooks.length > 0) {
+        setBooks(filteredBooks);
+        setMessage("");
+      } else {
+        setMessage("No such book found.");
+      }
 
-      const filteredBooks = res.items.filter((book) => {
-        return !book.saleInfo || book.saleInfo.isEbook;
-      });
-      setFreeBooks(filteredBooks);
+      //   const filteredBooks = res.items.filter((book) => {
+      //     return !book.saleInfo || book.saleInfo.isEbook;
+      //   });
+      //   setFreeBooks(filteredBooks);
     } catch (error) {
       console.error(error);
     }
   };
+
 
   return (
     <div>
@@ -50,30 +58,48 @@ export const Books = () => {
           Search
         </button>
       </form>
-      {freeBooks.length > 0 ? (
+      {books.length > 0 ? (
         <ul>
-          {freeBooks.map((book) => (
-            <li key={book.id}>
-              <h3>{book.volumeInfo.title}</h3>
-              <h3>{book.volumeInfo.imageLinks.thumbnail}</h3>
-              <img
+          {books.map((book) => (
+            <li key={book.key}>
+              {/* <h3>{book.volumeInfo.title}</h3> */}
+              <h2>{book.title}</h2>
+
+              {/* <h3>{book.volumeInfo.imageLinks.thumbnail}</h3> */}
+              {/* <img
                 src={book.volumeInfo.imageLinks.thumbnail}
                 alt="Book Thumbnail"
-              />
-              <p>Author: {book.volumeInfo.authors}</p>
-              <p>Description: {book.volumeInfo.description}</p>
+              /> */}
+              {/* <p>Author: {book.volumeInfo.authors}</p> */}
+              <p>Author: {book.author_name?.[0]}</p>
+              {/* <p>Description: {book.volumeInfo.description}</p> */}
+              <p>First Published: {book.first_publish_year}</p>
+              {book.cover_i && (
+                <img
+                  src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+                  alt="Book Cover"
+                />
+              )}
               <a
-                href={book.volumeInfo.previewLink}
+                href={`https://openlibrary.org${book.key}`}
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Read
               </a>
+
+              {/* <a
+                href={book.volumeInfo.previewLink}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Read
+              </a> */}
             </li>
           ))}
         </ul>
       ) : (
-        <p>No free books found.</p>
+        <p>{message}</p>
       )}
     </div>
   );
