75899ea917e1a5a59181059ae89f03568db6bf27
alajido <96017433+Alajido@users.noreply.github.com>
2023-06-12 04:56:37 +0100

    making some adjustment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,10 +25,10 @@ export const Hero = () => {
   }, []);
 
   return (
-    <section className="bg-[#212327] relative w-full min-h-[600px] py-16 md:py-20 px-[1.9rem] md:px-4">
+    <section className="bg-[#212327] relative w-full min-h-[600px] py-16 md:py-20 px-[1.8rem] md:px-4">
       <div className="container flex items-center justify-center mt-14 flex-col-reverse md:mt-20 md:flex-row mx-auto md:w-[75%]">
         <img
-          className="absolute md:hidden top-[12rem] -right-[1rem] mobile"
+          className="absolute md:hidden top-[12rem] -right-[1.2rem] mobile"
           src={mobilehero}
           alt="mobilehero"
         />
