765d48cb7a9b6018afe54aa58678e36fac349b88
alajido <96017433+Alajido@users.noreply.github.com>
2023-06-19 09:13:10 +0100

    making some adjustment

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -10,7 +10,8 @@ import { Sixth } from "./Sixth";
 import { Feedback } from "./Feedback";
 import { Footer } from "./Footer";
 import { motion } from "framer-motion";
-import {AiOutlineToTop} from "react-icons/ai"
+// import {AiOutlineToTop} from "react-icons/ai";
+import { ScrollBtn } from "./ScrollBtn";
 
 export const HomePage = () => {
   // initial={{ opacity: 0, scale: 0.5 }}
@@ -39,10 +40,12 @@ export const HomePage = () => {
       <SecondSection />
       <ThirdSection />
       <FourthSection />
-      <button className="text-red-500 text-lg fixed z-40 top-[70%]"><AiOutlineToTop /></button>
+
+      {/* <button className="text-red-500 text-lg fixed z-40 top-[70%]"><AiOutlineToTop /></button> */}
       <Fifth /> 
       <Sixth />
       <Feedback />
+      <ScrollBtn />
       <Footer className="relative" />
     </motion.div>
   );
